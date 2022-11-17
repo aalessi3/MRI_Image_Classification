fe7cfaf12927715fad26b6bdca89e27d5b73bc29
kedsuzuki <kedsuzuki820@gmail.com>
2022-11-16 21:29:55 -0500

    template for self attention

---
+++ src/SelfAttention.py
@@ -0,0 +1,20 @@
+import pytorch
+class SelfAttention(Module):
+    "Self attention layer for `n_channels`."
+    def __init__(self, n_channels):
+        self.query,self.key,self.value = [self._conv(n_channels, c) for c in (n_channels//8,n_channels//8,n_channels)]
+        self.gamma = nn.Parameter(tensor([0.]))
+
+    def _conv(self,n_in,n_out):
+        return ConvLayer(n_in, n_out, ks=1, ndim=1, norm_type=NormType.Spectral, act_cls=None, bias=False)
+
+    def forward(self, x):
+        #Notation from the paper.
+        size = x.size()
+        x = x.view(*size[:2],-1)
+        f,g,h = self.query(x),self.key(x),self.value(x)
+        beta = F.softmax(torch.bmm(f.transpose(1,2), g), dim=1)
+        o = self.gamma * torch.bmm(h, beta) + x
+        return o.view(*size).contiguous()
+
+view rawgistfile1.py hosted with ❤ by GitHub
